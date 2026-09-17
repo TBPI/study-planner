cf87654b7ac79fd13e9edf9e9fe1912a0288ff9f
TBPI <baumann.thilo@proton.me>
2026-09-17 13:36:25 +0200

    fix: separate style from html

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 from gemini_client import GeminiClient
 
-app = Flask(__name__, template_folder = '../templates')
+app = Flask(__name__, template_folder = '../templates', static_folder = 'static')
 client = GeminiClient()
 
 @app.route('/')
